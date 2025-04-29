e562bd6ae93196544a5f40030feea8e1af4eeb11
Ryan Kasper <ryancoreykasper@gmail.com>
2025-04-29 15:53:45 -0700

    Update problem4.py
    solution added

--- Mod4/problem4.py
+++ Mod4/problem4.py
@@ -1,1 +1,12 @@
+''' 4. A theater wants to enforce age restrictions for movie tickets.
+Ask the user for their age, and tell them whether they can see G (appropriate for under 13),
+PG-13 (appropriate for 13 to 17), or R (appropriate for over 18) rated movies.
 
+age = int(input("Enter your age: "))
+
+if age < 13:
+    print("You can see G rated movies.")
+elif age <= 17:
+    print("You can see PG-13 rated movies.")
+else:
+    print("You can see R rated movies.")
