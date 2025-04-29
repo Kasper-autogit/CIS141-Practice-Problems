4968b8c437d430c3f65963f7d1eb1c988a1a8c91
Ryan Kasper <ryancoreykasper@gmail.com>
2025-04-29 15:52:11 -0700

    Update problem3.py

--- Mod4/problem3.py
+++ Mod4/problem3.py
@@ -1,1 +1,7 @@
+''' 3. Prompt the user for their bank balance.
+Evaluate whether the balance is less than $100. 
+Print True if the user’s balance is below $100; print False, otherwise.'''
 
+balance = float(input("Enter your bank balance: "))
+
+print(balance < 100)
