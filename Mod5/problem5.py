4c56b7265a3b128e4d625d025fd61c0b2a1dc6dd
Ryan Kasper <ryancoreykasper@gmail.com>
2025-05-07 16:17:03 -0700

    Update problem5.py
    solution added

--- Mod5/problem5.py
+++ Mod5/problem5.py
@@ -1,1 +1,10 @@
+'''5. Write a program that continuously asks the user to input a number. 
+If the user enters 0, immediately stop asking for more numbers. 
+Otherwise, print the number they entered. Sample interaction:'''
+
+while True:
+    num = int(input("Enter a number (0 to stop): "))
+    if num == 0:
+        break
+    print("You entered", num)
 
