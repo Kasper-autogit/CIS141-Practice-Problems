8c7327587b51e13ef5e52f716e5d7b343a8e56a2
Ryan Kasper <ryancoreykasper@gmail.com>
2025-05-14 15:18:43 -0700

    Update problem5.py
    Solution added

--- Mod6/problem5.py
+++ Mod6/problem5.py
@@ -1,1 +1,11 @@
+'''#5. Create a list of integers.
+Use a loop to build a new list where each element is the square
+of the corresponding element in the original list. 
+Print the new list.
+'''
 
+list = [1, 2, 3, 4, 5]
+squares = []
+for element in list:
+    squares.append(element ** 2)
+print("Squares of the first list:", squares)
