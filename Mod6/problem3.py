ed335a69e4ea181a1e27aab79f03e6157340fd32
Ryan Kasper <ryancoreykasper@gmail.com>
2025-05-14 15:11:17 -0700

    Update problem3.py
    Solution added

--- Mod6/problem3.py
+++ Mod6/problem3.py
@@ -1,1 +1,11 @@
+'''#3. Create a list of strings. 
+Write code to create a new list that includes only the strings longer than three characters.
+Print the resulting filtered list.
+'''
 
+strings = ["fun", "fishing", "fat", "loud one", "star", "galaxy"]
+long_strings = []
+for s in strings:
+    if len(s) > 3:
+        long_strings.append(s)
+print("Strings longer than three characters:", long_strings)
