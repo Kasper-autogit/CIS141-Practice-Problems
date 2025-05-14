3e4d1ced4cabbc05ea7bdab9c9f47988069ee67a
Ryan Kasper <ryancoreykasper@gmail.com>
2025-05-14 15:09:11 -0700

    Update problem2.py
    Solution added

--- Mod6/problem2.py
+++ Mod6/problem2.py
@@ -1,1 +1,11 @@
+'''#2. Create a list of strings. 
+Write code that counts how many times the word 
+"Olympic" appears in the list, 
+and then print the count.'''
 
+word = ["Olympic", "College", "Washington", "Fishing"]
+olympic_count = 0
+for element in word:
+    if element == "Olympic":
+        olympic_count += 1
+print("Count of 'Olympic':", olympic_count)
