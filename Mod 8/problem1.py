d396366dbfc706b9584564948b24a31f4962fe5b
Ryan Kasper <ryancoreykasper@gmail.com>
2025-05-29 13:35:26 -0700

    Update problem1.py

--- Mod 8/problem1.py
+++ Mod 8/problem1.py
@@ -1,1 +1,12 @@
-
+'''
+#1. Create a file called gardening_tips.txt and add at least 3 gardening tips to
+it. Write a Python script that reads a file gardening_tips.txt and prints
+out each tip one by one.
+'''
+with open('gardening_tips.txt', 'w') as file:
+    file.write("Water your plants early in the morning.\n")
+    file.write("Use compost to enrich your soil.\n")
+    file.write("Get a lot of sun.\n")
+with open('gardening_tips.txt', 'r') as file:
+    for line in file:
+        print("Tip:", line.strip())
